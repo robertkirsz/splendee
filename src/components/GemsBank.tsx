a7b42e208df5192a33d6db596681df8c8d9f5c90
Robert Kirsz <robert.kirsz@gmail.com>
2020-11-10 10:25:51 +0100

    Add some UI placeholders

---
+++ src/components/GemsBank.tsx
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export default function GemsBank() {
+  return <div>Gems Bank</div>
+}
---
+++ src/components/NoblesRow.tsx
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export default function NoblesRow() {
+  return <div>Nobles Row</div>
+}
