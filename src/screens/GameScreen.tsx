75b5894f9dfca52ef3a2a19b93fe55c6dd5fdb53
Robert Kirsz <robert.kirsz@gmail.com>
2021-03-19 23:48:52 +0100

    Disable screen when non-active

--- src/screens/GameScreen.tsx
+++ src/screens/GameScreen.tsx
@@ -1,15 +1,28 @@
+import { useContext } from 'react'
+import { observer } from 'mobx-react-lite'
+
 import GemsBank from 'components/GemsBank'
 import NoblesRow from 'components/NoblesRow'
 import CardsBoard from 'components/CardsBoard'
 import GameControls from 'components/GameControls'
 import PlayersSection from 'components/PlayersSection'
 
-export default function GameScreen() {
+import { playerStore, gameStore } from 'store'
+
+export default observer(function GameScreen() {
+  const player = useContext(playerStore)
+  const game = useContext(gameStore)
+
+  const notYouTurn = game.activePlayerId !== player.id
+
   return (
     <div className="flex justify-between">
       <PlayersSection />
 
-      <div className="flex flex-col space-y-4">
+      <div
+        className="flex flex-col space-y-4"
+        style={notYouTurn ? { opacity: 0.5, pointerEvents: 'none' } : undefined}
+      >
         <NoblesRow />
 
         <div className="flex items-center space-x-4">
@@ -21,4 +34,4 @@ export default function GameScreen() {
       </div>
     </div>
   )
-}
+})
