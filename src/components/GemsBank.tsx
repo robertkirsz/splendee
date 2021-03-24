57f9803056cf5fe7958b4e36614a501c999d36a8
Robert Kirsz <robert.kirsz@gmail.com>
2021-03-24 11:44:43 +0100

    Add labels

--- src/components/GemsBank.tsx
+++ src/components/GemsBank.tsx
@@ -12,11 +12,52 @@ import { playerStore, gameStore } from 'store'
 import { getGemColor, sc } from 'utils'
 import socket from 'socket'
 
+const Label = styled.span`
+  height: 20px;
+  padding: 4px;
+
+  position: absolute;
+  right: calc(100% + 16px);
+  top: 50%;
+
+  background: white;
+  border-radius: 4px 0 0 4px;
+
+  font-size: 11px;
+  line-height: 1;
+  white-space: nowrap;
+
+  pointer-events: none;
+  filter: drop-shadow(0 5px 3px rgba(0, 0, 0, 0.7));
+
+  opacity: 0;
+  transform: translate(-8px, -50%);
+  transition-property: opacity, transform;
+  transition-duration: 100ms;
+
+  &::after {
+    content: '';
+    width: 16px;
+    height: 16px;
+
+    position: absolute;
+    top: 50%;
+    right: -8px;
+    z-index: -1;
+
+    background: inherit;
+    border-radius: 3px;
+
+    transform: rotate(46deg) translateY(-50%);
+    transform-origin: top;
+  }
+`
+
 export default observer(function GemsBank() {
   const player = useContext(playerStore)
-  const { roomId, gems, earnGem, giveTurnToNextPlayer } = useContext(gameStore)
+  const { roomId, gems, earnGem, giveTurnToNextPlayer, activePlayerId } = useContext(gameStore)
   const { chosenGems, chooseGem, clearChoosenGems } = useContext(playerStore)
-  const _gems = toJS(chosenGems)
+  const _chosenGems = toJS(chosenGems)
 
   function handleClick(color: GemColorsType) {
     return () => {
@@ -26,8 +67,11 @@ export default observer(function GemsBank() {
   }
 
   useEffect(() => {
-    if (_gems.length === 3 || (_gems.length === 2 && _gems[0] === _gems[1])) {
-      socket.emitSyncGems(roomId, player.id, _gems)
+    if (
+      _chosenGems.length === 3 ||
+      (_chosenGems.length === 2 && _chosenGems[0] === _chosenGems[1])
+    ) {
+      socket.emitSyncGems(roomId, player.id, _chosenGems)
       socket.emitSendMessage(roomId, {
         type: MessageTypes.Gems,
         text: `${player.name} took these gems`,
@@ -36,30 +80,58 @@ export default observer(function GemsBank() {
       clearChoosenGems()
       giveTurnToNextPlayer()
     }
-  }, [_gems, chosenGems, clearChoosenGems, giveTurnToNextPlayer, player.id, player.name, roomId])
+  }, [_chosenGems, chosenGems, clearChoosenGems, giveTurnToNextPlayer, player.id, player.name, roomId])
 
   const gemColors = Object.keys(gems) as GemColorsType[]
 
   return (
-    <Div columnTop selfCenter>
-      {gemColors.map(color => (
-        <GemContainer
-          key={color}
-          color={color}
-          onClick={handleClick(color)}
-          hidden={!gems[color]}
-          disabled={_gems.includes(color) && (_gems.length > 1 || gems[color] <= 2)}
-          data-gem-container-color={color}
-          css={`
-            &:last-child {
-              margin-top: 80px;
-              pointer-events: none;
-            }
-          `}
-        >
-          {gems[color]}
-        </GemContainer>
-      ))}
+    <Div
+      columnTop
+      selfCenter
+      // @ts-ignore
+      css={`
+        &:hover ${Label} {
+          opacity: 1;
+          transform: translate(0, -50%);
+          transition-delay: 1s;
+        }
+      `}
+    >
+      {gemColors.map(color => {
+        const isDisabled =
+          _chosenGems.includes(color) && (_chosenGems.length > 1 || gems[color] <= 2)
+        const labelVisible =
+          activePlayerId === player.id &&
+          chosenGems.length === 0 &&
+          gems[color] > 1 &&
+          gems[color] < 4
+
+        return (
+          <Div
+            key={color}
+            relative
+            // @ts-ignore
+            css={`
+              &:last-child {
+                margin-top: 80px;
+                pointer-events: none;
+              }
+            `}
+          >
+            <GemContainer
+              color={color}
+              onClick={handleClick(color)}
+              hidden={!gems[color]}
+              disabled={isDisabled}
+              data-gem-container-color={color}
+            >
+              {gems[color]}
+            </GemContainer>
+
+            {labelVisible && <Label>Only one</Label>}
+          </Div>
+        )
+      })}
     </Div>
   )
 })
